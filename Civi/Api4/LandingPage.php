3999f17bea374bd49014d08bdef7c8c0b987ae92
Rich Lott / Artful Robot <code.commits@artfulrobot.uk>
2025-03-21 13:32:44 +0000

    Add permissions: view|administer

--- Civi/Api4/LandingPage.php
+++ Civi/Api4/LandingPage.php
@@ -10,4 +10,13 @@ namespace Civi\Api4;
  */
 class LandingPage extends Generic\DAOEntity {
 
+
+  public static function permissions() {
+    return [
+      'meta' => ['access CiviCRM'],
+      'default' => ['administer landing pages'],
+      'get' => ['view landing pages'],
+    ];
+  }
+
 }
